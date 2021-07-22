90b494be59a36bb9e80f06547e7be14c68092117
phoaiphuthinh <phoaiphuthinh@gmail.com>
2021-07-22 22:15:33 +0700

    fix avatar

--- routers/file.js
+++ routers/file.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
+const Tutor = require('../models/tutor')
+const Course = require('../models/course')
 const auth = require('../middlewares/auth')
 const router = express.Router()
 const multer = require('multer')
